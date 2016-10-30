a18be7be64769a6e7536da4821c47198dd9d0225
Tony Wang <jiaweitwang@gmail.com>
2016-10-29 18:07:24 -0700

    documentation

--- server/src/github.com/exogig/db/database.go
+++ server/src/github.com/exogig/db/database.go
@@ -2,6 +2,7 @@ package db
 
 import "github.com/garyburd/redigo/redis"
 
+// Opens a connection to the redis database using the redigo library
 func Connect() redis.Conn {
 	connection, err := redis.Dial("tcp", ":6379")
 	if err != nil {
@@ -10,6 +11,7 @@ func Connect() redis.Conn {
 	return connection
 }
 
+// Closes the connection to the database
 func Disconnect(connection redis.Conn) {
 	connection.Close()
 }
