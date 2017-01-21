d5f233cc5b2bf0d3f35b6a5884177a6450c038c0
Tony Wang <tony@ghost.local>
2017-01-21 15:27:55 -0800

    Completed data structures for frontend gig

--- client-build/src/app/gig/app.gig.gig.ts
+++ client-build/src/app/gig/app.gig.gig.ts
@@ -13,9 +13,77 @@
 export class Song {
   name: string;
   rating: number;
+  getName() {
+    return this.name;
+  }
+  getRating() {
+    return this.rating;
+  }
 }
 
 export class SongList {
   listName: string;
   songs: Song[];
+  getName() {
+    return this.listName;
+  }
+  getSongs() {
+    return this.songs;
+  }
+}
+
+export class Set {
+  songsInSet: Song;
+  setName: string;
+  getSongsInSet() {
+    return this.songsInSet;
+  }
+  getSetName() {
+    return this.setName;
+  }
+}
+
+export class SetList {
+  setsInSetList: Set;
+  setListName: string;
+  getSetsInSetList() {
+    return this.setsInSetList;
+  }
+  getSetListName() {
+    return this.setListName;
+  }
+}
+
+export class Request {
+  requestedSong: Song;
+  getRequestedSong() {
+    return this.requestedSong;
+  }
+}
+
+export class Gig {
+  gigSetList: SetList;
+  gigName: string;
+  gigTime: string;
+  gigDate: string;
+  gigLocation: string;
+  gigRequestList: Request[];
+  getGigSetList() {
+    return this.gigSetList;
+  }
+  getGigName() {
+    return this.gigName;
+  }
+  getGigTime() {
+    return this.gigTime;
+  }
+  getGigDate() {
+    return this.gigDate;
+  }
+  getGigLocation() {
+    return this.gigLocation;
+  }
+  getGigRequestList() {
+    return this.gigRequestList;
+  }
 }
