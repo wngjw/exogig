e21d338a1ae91e50bcea2c216a0f4196ead2ed64
Spencer Zag <Spencer Zag>
2017-02-08 18:07:35 -0800

    Reupload

--- client-build/src/app/app.component.login.ts
+++ client-build/src/app/app.component.login.ts
@@ -1,5 +1,5 @@
 import { Input, Output, Component, Directive, Injectable, EventEmitter } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+import { Headers, Http, URLSearchParams, RequestOptions } from '@angular/http';
 import { MaterializeAction } from 'angular2-materialize';
 import { GoogleAPILoader } from './gapi/app.gapi.gapiloader'
 
@@ -12,8 +12,10 @@ import { GoogleAPILoader } from './gapi/app.gapi.gapiloader'
 export class AppLoginComponent {
 	notify: EventEmitter<string> = new EventEmitter<string>();
   loginhtml: string;
-	inputKey: string;		//On page value
+	inputKey: string;		//On page <input> value
+	entireGigObject: Object;
   promise = GoogleAPILoader.load();
+	http: Http;
 
 	modalActions = new EventEmitter<string|MaterializeAction>();
   	openModal() {
@@ -23,14 +25,37 @@ export class AppLoginComponent {
     	this.modalActions.emit({action:"modal",params:['close']});
   	}
 
-	constructor() {
+	constructor(http: Http) {
+		this.http = http;
 	}
 
 
 	public joinEvent(location:string) {
 		console.log(this.inputKey) 
-	
-	
+		
+		var uploadObj = {
+			key: this.inputKey
+		};
+
+		let params = new URLSearchParams;
+
+		for(let key in uploadObj) {						//This key is like the key in a dictionary. It represents the object's names for each value (each value being the actual reference of the dictionary's key)
+			params.set(key, uploadObj[key]);
+		}
+		let options = new RequestOptions({
+			search: params
+		});
+
+		this.http.get('/kendrick', options).map(res => res.json()).subscribe(data => this.entireGigObject = data);
+    
+		if(!typeof(this.entireGigObject) === undefined)
+		{
+			console.log("Recieved Gig!");
+			console.log(this.entireGigObject);
+		}
+			console.log(this.entireGigObject);
+		
+		this.notify.emit(location);
 	}
 
 
