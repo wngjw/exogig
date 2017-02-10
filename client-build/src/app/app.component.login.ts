c7810b619e58e49b0cafaf97fec7b8c3ca52e0a8
Spencer Zag <Spencer Zag>
2017-02-10 01:04:19 -0800

    Wrote gig ID checking functionality Implemented changes on the login.ts file and server.go file. The login.ts file uses an http.GET to send the key value on the query params of the URI. The server.go file recieves the http.GET, retrieves the key value on the params of the URI, then checks it against our predefined Gig. If it's a match, it'll return the Gig. If not, it'll return null.
    Movement to other pages has been commented out for testing purposes. See
    app.component.login.ts to remove the comment for movement across the
    app.

--- client-build/src/app/app.component.login.ts
+++ client-build/src/app/app.component.login.ts
@@ -37,26 +37,31 @@ export class AppLoginComponent {
 			key: this.inputKey
 		};
 
-		console.log(JSON.stringify(uploadObj))
+    let params: URLSearchParams = new URLSearchParams();
+
+		for(let key in uploadObj) {
+			params.set(key, uploadObj[key]);
+		}
+		//console.log(JSON.stringify(uploadObj))
 
 		var headers = new Headers();
 		headers.append('Content-Type','application/json');
 
 		let options = new RequestOptions({
-			headers: headers,
-			body: JSON.stringify(uploadObj)
+			search: params
 		});
 
 		this.http.get('/kendrick', options).map(res => res.json()).subscribe(data => this.entireGigObject = data);
 
-		if(!typeof(this.entireGigObject) === undefined)
-		{
-			console.log("Recieved Gig!");
-			console.log(this.entireGigObject);
-		}
-			console.log(this.entireGigObject);
+		//This will be undefined since it runs asynchronously. BUT if you press the button again, it'll be populated.
+		console.log(this.entireGigObject);
 
-		this.notify.emit(location);
+		//Currently I'm commenting this out since the Gig object is kept in scope of this one page only.
+		//We'll need to provide access to this object through either an Angular2 service, or passing it through the emit function below.
+		//If you need to test other pages just uncomment this.notify.emit below, BUT as of 2/10 the Gig object will not
+		//be in scope on the other pages until the above is created and implemented.
+		
+		//this.notify.emit(location);
 	}
 
   public signOut() {
