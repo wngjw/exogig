c4cedf71ef2f18012e0b4cffb3ae4932f38085d5
Tony Wang <tony@ghost.local>
2017-02-18 16:51:27 -0800

    formatted module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
 
+/* Component files */
 import { AppLoginComponent } from './component/app.component.login';
 import { AppControllerComponent } from './component/app.component.controller';
 import { AppMainComponent } from './component/app.component.main'
@@ -21,54 +22,54 @@ import { AppSetComponent } from './component/app.component.set';
 import { AppSongComponent } from './component/app.component.song';
 import { AppCreateSetsComponent } from './component/app.component.makesets';
 
+/* Material design */
 import { MaterializeDirective } from "angular2-materialize";
-import { MdButton,MdButtonModule,MdAnchor } from '@angular2-material/button'
-import { MdIcon,MdIconModule,MdIconRegistry } from '@angular2-material/icon'
-import { MdInput,MdInputModule } from '@angular2-material/input'
-import { MdTab,MdTabsModule,MdTabGroup,MdTabChangeEvent } from '@angular2-material/tabs'
-//Consider filtering unnecessary imported modules.
-import { MdCard,MdCardActions,MdCardContent,MdCardHeader,MdCardModule,MdCardTitle,MdCardSubtitle,MdCardTitleGroup } from '@angular2-material/card'
-import { MdGridList,MdGridListModule } from '@angular2-material/grid-list'
+import { MdButton, MdButtonModule, MdAnchor } from '@angular2-material/button'
+import { MdIcon, MdIconModule, MdIconRegistry } from '@angular2-material/icon'
+import { MdInput, MdInputModule } from '@angular2-material/input'
+import { MdTab, MdTabsModule, MdTabGroup, MdTabChangeEvent } from '@angular2-material/tabs'
+import { MdCard, MdCardActions, MdCardContent, MdCardHeader, MdCardModule, MdCardTitle, MdCardSubtitle, MdCardTitleGroup } from '@angular2-material/card'
+import { MdGridList, MdGridListModule } from '@angular2-material/grid-list'
 
 import { gigService } from './services/app.service.gig';
 import { userService } from './services/app.service.user';
 
 @NgModule({
-  imports:      [
-  	BrowserModule,
-  	FormsModule,
-  	HttpModule,
-	MdButtonModule,
-	MdIconModule,
-	MdInputModule,
-	MdTabsModule,
-	MdCardModule,
-	MdGridListModule,
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MdButtonModule,
+    MdIconModule,
+    MdInputModule,
+    MdTabsModule,
+    MdCardModule,
+    MdGridListModule,
   ],
   declarations: [
-	AppMainComponent,
-  	AppLoginComponent,
-  	AppControllerComponent,
-  	AppMainComponent,
-  	AppLoginComponent,
-  	AppControllerComponent,
-  	AppSongListComponent,
-  	AppBandPageComponent,
-  	AppBandViewerComponent,
-  	AppChatComponent,
-  	AppCreateEventComponent,
-  	AppEditBioComponent,
-  	AppCreateSetlistComponent,
-  	AppRateComponent,
-  	AppRequestComponent,
-  	AppSetComponent,
-  	AppSongComponent,
-  	AppCreateSetsComponent,
-	MaterializeDirective,
+    AppMainComponent,
+    AppLoginComponent,
+    AppControllerComponent,
+    AppMainComponent,
+    AppLoginComponent,
+    AppControllerComponent,
+    AppSongListComponent,
+    AppBandPageComponent,
+    AppBandViewerComponent,
+    AppChatComponent,
+    AppCreateEventComponent,
+    AppEditBioComponent,
+    AppCreateSetlistComponent,
+    AppRateComponent,
+    AppRequestComponent,
+    AppSetComponent,
+    AppSongComponent,
+    AppCreateSetsComponent,
+    MaterializeDirective,
 
-   ],
-   bootstrap:	[ AppControllerComponent ],	//From what I understand, bootstrap is basically which component it tries to setup first.
-   providers: [MdIconRegistry, gigService, userService]
+  ],
+  bootstrap: [AppControllerComponent],	//From what I understand, bootstrap is basically which component it tries to setup first.
+  providers: [MdIconRegistry, gigService, userService]
 })
 
-export class AppModule {}
+export class AppModule { }
